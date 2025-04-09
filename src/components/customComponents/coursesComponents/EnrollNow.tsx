ca6e78e4447c1e750ff40e2044bf276215c18f24
Sumit <tonystark83033@gmail.com>
2025-04-09 17:47:14 +0530

    add rupe symbol

--- src/components/customComponents/coursesComponents/EnrollNow.tsx
+++ src/components/customComponents/coursesComponents/EnrollNow.tsx
@@ -7,8 +7,8 @@ const EnrollNow = ({ course }:any) => {
     <div className="fixed bottom-0 left-0 w-full h-[90px]  z-50 flex md:hidden lg:hidden items-center justify-center ">
      <div className="flex items-center justify-between h-full bg-[#0F0000] text-white p-2 w-full">
       <div className="flex items-center gap-2">
-        <span className="text-[#FF0000] text-xl">{course.price}</span>
-        <span className="text-xs text-gray-400 line-through">{course.originalPrice}</span>
+        <span className="text-[#FF0000] text-xl">₹{course.price}+Taxes</span>
+        <span className="text-xs text-gray-400 line-through">₹{course.originalPrice}</span>
       </div>
       <a href={course.paymentLink}>
       <Button className="bg-[#FF0000] hover:bg-[#E60000] text-white rounded-sm text-xl px-4 py-1 h-auto font-normal">
