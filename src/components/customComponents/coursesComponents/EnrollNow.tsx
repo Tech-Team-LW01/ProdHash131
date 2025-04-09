548006a4ab80364752a82d72e5b5cec89849479f
Sumit <tonystark83033@gmail.com>
2025-04-09 12:56:56 +0530

    add price section in mobile

---
+++ src/components/customComponents/coursesComponents/EnrollNow.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button } from '@/components/ui/button'; /// Assuming Shadcn is correctly set up and Button component is available
+
+const EnrollNow = ({ course }:any) => {
+  return (
+    // Container for the button with Tailwind's responsive utilities
+    <div className="fixed bottom-0 left-0 w-full h-[60px] bg-[#ff0000] z-50 flex md:hidden lg:hidden items-center justify-center ">
+     <div className="flex items-center justify-between bg-[#0F0000] text-white p-2 w-full">
+      <div className="flex items-center gap-2">
+        <span className="text-[#FF0000] font-medium">{course.price}</span>
+        <span className="text-xs text-gray-400 line-through">{course.originalPrice}</span>
+      </div>
+      <a href={course.paymentLink}>
+      <Button className="bg-[#FF0000] hover:bg-[#E60000] text-white rounded-none text-sm px-4 py-1 h-auto font-normal">
+        Reserve Seat
+      </Button>
+      </a>
+    </div>
+    </div>
+  );
+};
+
+export default EnrollNow;
