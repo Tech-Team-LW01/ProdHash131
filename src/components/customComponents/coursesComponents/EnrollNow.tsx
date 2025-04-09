ef7431aa18ae9a9c3b0bfa197fe681a7f9eb7b14
Sumit <tonystark83033@gmail.com>
2025-04-09 14:52:15 +0530

    add enroll now button

--- src/components/customComponents/coursesComponents/EnrollNow.tsx
+++ src/components/customComponents/coursesComponents/EnrollNow.tsx
@@ -4,14 +4,14 @@ import { Button } from '@/components/ui/button'; /// Assuming Shadcn is correctl
 const EnrollNow = ({ course }:any) => {
   return (
     // Container for the button with Tailwind's responsive utilities
-    <div className="fixed bottom-0 left-0 w-full h-[60px] bg-[#ff0000] z-50 flex md:hidden lg:hidden items-center justify-center ">
-     <div className="flex items-center justify-between bg-[#0F0000] text-white p-2 w-full">
+    <div className="fixed bottom-0 left-0 w-full h-[90px]  z-50 flex md:hidden lg:hidden items-center justify-center ">
+     <div className="flex items-center justify-between h-full bg-[#0F0000] text-white p-2 w-full">
       <div className="flex items-center gap-2">
-        <span className="text-[#FF0000] font-medium">{course.price}</span>
+        <span className="text-[#FF0000] text-xl">{course.price}</span>
         <span className="text-xs text-gray-400 line-through">{course.originalPrice}</span>
       </div>
       <a href={course.paymentLink}>
-      <Button className="bg-[#FF0000] hover:bg-[#E60000] text-white rounded-none text-sm px-4 py-1 h-auto font-normal">
+      <Button className="bg-[#FF0000] hover:bg-[#E60000] text-white rounded-sm text-xl px-4 py-1 h-auto font-normal">
         Reserve Seat
       </Button>
       </a>
