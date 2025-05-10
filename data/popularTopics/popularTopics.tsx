d3842185fe556c5b88117be0ad0a52ecb9a94500
anshuraghv12 <anshuraghav1204@gmail.com>
2025-05-10 13:34:46 +0530

    update categaries

--- data/popularTopics/popularTopics.tsx
+++ data/popularTopics/popularTopics.tsx
@@ -2,15 +2,15 @@ import { TopicCard } from "../../types/popularTopics";
 
 export const topics: TopicCard[] = [
     { title: "Devops", courses: 68, icon: "⌛" },
-    { title: "Programming", courses: 27, icon: "📐" },
-    { title: "DataBase", courses: 64, icon: "🌐" },
-    { title: "Cloud Computing", courses: 45, icon: "☁️" },
-    { title: "Cyber Security", courses: 32, icon: "🛡️" },
-    { title: "System Administration", courses: 54, icon: "🤖" },
-    { title: "Artificial Intelligence", courses: 32, icon: "🛡️" },
-    { title: "web development", courses: 54, icon: "🤖" },
-    { title: "Data Applications", courses: 64, icon: "🌐" },
-    { title: "Web Scraping & data extraction", courses: 45, icon: "☁️" },
-    { title: "Cyber Security", courses: 32, icon: "🛡️" },
+    { title: " Cloud & Cloud Computing", courses: 27, icon: "📐" },
+    { title: "Programming", courses: 64, icon: "🌐" },
+    { title: " Database", courses: 45, icon: "☁️" },
+    { title: "System Administration", courses: 32, icon: "🛡️" },
+    { title: "Artificial Intelligence", courses: 54, icon: "🤖" },
+    { title: "Web Development", courses: 32, icon: "🛡️" },
+    { title: "Data Applications", courses: 54, icon: "🤖" },
+    { title: " Web Scraping & Data Extraction", courses: 64, icon: "🌐" },
+    { title: " Cybersecurity & Linux", courses: 45, icon: "☁️" },
+    // { title: "Cyber Security", courses: 32, icon: "🛡️" },
   ];
   
